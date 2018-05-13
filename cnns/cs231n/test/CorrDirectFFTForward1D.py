71ad324e26493a0767d95ec533e8a0f9d44c299e
Adam Dziedzic <adam.cajf@gmail.com>
2018-05-13 10:56:49 -0500

    more checks for absolute error and convolution

--- cnns/cs231n/test/CorrDirectFFTForward1D.py
+++ cnns/cs231n/test/CorrDirectFFTForward1D.py
@@ -27,6 +27,10 @@ scipy_correlate = signal.correlate(x, filters, mode=mode)
 print("correlate scipy:", scipy_correlate)
 print("correlate scipy shape:", scipy_correlate.shape)
 
+scipy_fft = signal.correlate(x, filters, mode=mode, method="fft")
+print("fft scipy:", scipy_fft)
+print("fft scipy shape:", scipy_fft.shape)
+
 np_correlate = np.correlate(x, filters, mode=mode)
 print("correlate numpy:", np_correlate)
 print("correlate numpy shape:", np_correlate.shape)
@@ -39,8 +43,10 @@ filterfft = np.conj(np.fft.fft(filters, len(xfft)))
 out = xfft * filterfft
 # take the inverse of the output from the frequency domain and return the modules of the complex numbers
 out = np.fft.ifft(out)
-output = np.array(out, np.double)[:xfft.shape[-1]]
+output = np.real(out)[:xfft.shape[-1]]
+out_W = np.int(((len(x) + 2*padding - len(filters)) / stride) + 1)
 # output = np.absolute(out)
+output = output[:out_W]
 
 print("output of cross-correlation via fft: ", output)
 print("output of cross-correlation via fft shape: ", output.shape)
@@ -63,5 +69,8 @@ print("outfff shape: ", outfft.shape)
 
 print("is the fft cross_correlation for convolution correct with respect to naive: ",
       np.allclose(outfft, outnaive, atol=1e-12))
-print("absolute error fft naive: ", np.sum(np.abs(outfft - outnaive)))
 print("relative error fft naive: ", rel_error(outfft, outnaive))
+print("absolute error fft: ", np.sum(np.abs(outfft - outnaive)))
+print("absolute error scipy: ", np.sum(np.abs(scipy_correlate - outnaive)))
+print("absolute error numpy: ", np.sum(np.abs(np_correlate - outnaive)))
+print("absolute error scipy fft: ", np.sum(np.abs(scipy_fft - outnaive)))
