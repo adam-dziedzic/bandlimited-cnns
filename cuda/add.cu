0846e8e3f2503463e5a3b3ad99eb71537cec2dd5
Adam Dziedzic <ady@cs.uchicago.edu>
2018-06-19 08:18:59 -0500

    cuda first tests

---
+++ cuda/add.cu
@@ -0,0 +1,49 @@
+// device code - should run on GPU
+#include <iostream>
+#include <math.h>
+
+// function to add the elements of two arrays
+// CUDA Kernel function to add the elements of two arrays on the GPU
+__global__
+void add(int n, float *x, float *y) {
+  for (int i=0; i < n; ++i) {
+    y[i] = x[i] + y[i];
+  }
+}
+
+int main(void) {
+  int N = 1<<20; // 1M elements
+
+  float *x, *y;
+  // Allocate unified memory - accessible from cpu or gpu
+  cudaMallocManaged(&x, N*sizeof(float));
+  cudaMallocManaged(&y, N*sizeof(float));
+
+  // initialize x and y arrays on the host
+  for (int i=0; i<N; ++i) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+
+  // Run the kernel on 1M elements on the CPU.
+  // add(N, x, y);
+  add<<<1, 1>>>(N, x, y);
+
+  // Wait for GPU to finish before accessing on host (cpu)
+  cudaDeviceSynchronize();
+
+  // Check for errors (all values should be 3.0f)
+  float maxError = 0.0f;
+  for (int i=0; i<N; ++i) {
+    maxError = fmax(maxError, fabs(y[i] - 3.0f));
+  }
+  std::cout << "Max error: " << maxError << std::endl;
+
+  // Free memory.
+  // delete [] x;
+  // delete [] y;
+  cudaFree(x);
+  cudaFree(y);
+
+  return 0;
+}
---
+++ cuda/first_cuda.c~
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+__global__
+
+void saxpy(int n, float a, float *x, float *y)
+{
+  int i = blockIdx.x*blockDim.x + threadIdx.x;
+  if (i < n) y[i] = a*x[i] + y[i];
+}
+
+int main(void) {
+  int N = 1 << 20;
+  float *x, *y, *d_x, *d_y;
+  x = (float*)malloc(N*sizeof(float));
+  y = (float*)malloc(N*sizeof(float));
+
+  cudaMalloc(&d_x, N*sizeof(float));
+  cudaMalloc(&d_y, N*sizeof(float));
+
+  
+}
