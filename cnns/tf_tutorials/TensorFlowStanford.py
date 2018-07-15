4870631075a2e0c92d7531c39f1504a8808753f3
Adam Dziedzic <adam.cajf@gmail.com>
2018-07-14 20:23:45 -0500

    alex net in tensorflow

--- cnns/tf_tutorials/TensorFlowStanford.py
+++ cnns/tf_tutorials/TensorFlowStanford.py
@@ -1,8 +1,9 @@
-import tensorflow as tf
-import numpy as np
 import math
-import timeit
+
 import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
 from nnlib.data_utils import load_CIFAR10
 
 
@@ -13,7 +14,7 @@ def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=10000):
     we used for the SVM, but condensed to a single function.  
     """
     # Load the raw CIFAR-10 data
-    cifar10_dir = 'nnlib/datasets/cifar-10-batches-py'
+    cifar10_dir = '../nnlib/datasets/cifar-10-batches-py'
     X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
 
     # Subsample the data
@@ -46,6 +47,7 @@ def check_CIFAR10_data():
     print('Test data shape: ', X_test.shape)
     print('Test labels shape: ', y_test.shape)
 
+
 # clear old variables
 tf.reset_default_graph()
 
@@ -55,9 +57,10 @@ X = tf.placeholder(tf.float32, [None, 32, 32, 3])
 y = tf.placeholder(tf.int64, [None])
 is_training = tf.placeholder(tf.bool)
 
-def simple_model(X,y):
+
+def simple_model(X, y):
     # define our weights (e.g. init_two_layer_convnet)
-    
+
     # setup variables
     Wconv1 = tf.get_variable("Wconv1", shape=[7, 7, 3, 32])
     bconv1 = tf.get_variable("bconv1", shape=[32])
@@ -66,41 +69,43 @@ def simple_model(X,y):
     b1 = tf.get_variable("b1", shape=[10])
 
     # define our graph (e.g. two_layer_convnet)
-    a1 = tf.nn.conv2d(X, Wconv1, strides=[1,2,2,1], padding='VALID') + bconv1
+    a1 = tf.nn.conv2d(X, Wconv1, strides=[1, 2, 2, 1], padding='VALID') + bconv1
     h1 = tf.nn.relu(a1)
-    h1_flat = tf.reshape(h1,[-1,5408]) # whatever number of data points
-    y_out = tf.matmul(h1_flat,W1) + b1
+    h1_flat = tf.reshape(h1, [-1, 5408])  # whatever number of data points
+    y_out = tf.matmul(h1_flat, W1) + b1
     return y_out
 
-y_out = simple_model(X,y)
+
+y_out = simple_model(X, y)
 
 # define our loss
-total_loss = tf.losses.hinge_loss(tf.one_hot(y,10),logits=y_out)
+total_loss = tf.losses.hinge_loss(tf.one_hot(y, 10), logits=y_out)
 mean_loss = tf.reduce_mean(total_loss)
 
 # define our optimizer
-optimizer = tf.train.AdamOptimizer(5e-4) # select optimizer and set learning rate
+optimizer = tf.train.AdamOptimizer(5e-4)  # select optimizer and set learning rate
 train_step = optimizer.minimize(mean_loss)
+
 
 def run_model(session, predict, loss_val, Xd, yd,
               epochs=1, batch_size=64, print_every=100,
               training=None, plot_losses=False):
     # have tensorflow compute accuracy
-    correct_prediction = tf.equal(tf.argmax(predict,1), y)
+    correct_prediction = tf.equal(tf.argmax(predict, 1), y)
     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    
+
     # shuffle indicies
     train_indicies = np.arange(Xd.shape[0])
     np.random.shuffle(train_indicies)
 
     training_now = training is not None
-    
+
     # setting up variables we want to compute (and optimizing)
     # if we have a training function, add that to things we compute
-    variables = [mean_loss,correct_prediction,accuracy]
+    variables = [mean_loss, correct_prediction, accuracy]
     if training_now:
         variables[-1] = training
-    
+
     # counter 
     iter_cnt = 0
     for e in range(epochs):
@@ -108,78 +113,79 @@ def run_model(session, predict, loss_val, Xd, yd,
         correct = 0
         losses = []
         # make sure we iterate over the dataset once
-        for i in range(int(math.ceil(Xd.shape[0]/batch_size))):
+        for i in range(int(math.ceil(Xd.shape[0] / batch_size))):
             # generate indicies for the batch
-            start_idx = (i*batch_size)%Xd.shape[0]
-            idx = train_indicies[start_idx:start_idx+batch_size]
-            
+            start_idx = (i * batch_size) % Xd.shape[0]
+            idx = train_indicies[start_idx:start_idx + batch_size]
+
             # create a feed dictionary for this batch
-            feed_dict = {X: Xd[idx,:],
+            feed_dict = {X: Xd[idx, :],
                          y: yd[idx],
-                         is_training: training_now }
+                         is_training: training_now}
             # get batch size
             actual_batch_size = yd[idx].shape[0]
-            
+
             # have tensorflow compute loss and correct predictions
             # and (if given) perform a training step
-            loss, corr, _ = session.run(variables,feed_dict=feed_dict)
-            
+            loss, corr, _ = session.run(variables, feed_dict=feed_dict)
+
             # aggregate performance stats
-            losses.append(loss*actual_batch_size)
+            losses.append(loss * actual_batch_size)
             correct += np.sum(corr)
-            
+
             # print every now and then
             if training_now and (iter_cnt % print_every) == 0:
-                print("Iteration {0}: with minibatch training loss = {1:.3g} and accuracy of {2:.2g}"\
-                      .format(iter_cnt,loss,np.sum(corr)/actual_batch_size))
+                print("Iteration {0}: with minibatch training loss = {1:.3g} and accuracy of {2:.2g}" \
+                      .format(iter_cnt, loss, np.sum(corr) / actual_batch_size))
             iter_cnt += 1
-        total_correct = correct/Xd.shape[0]
-        total_loss = np.sum(losses)/Xd.shape[0]
-        print("Epoch {2}, Overall loss = {0:.3g} and accuracy of {1:.3g}"\
-              .format(total_loss,total_correct,e+1))
+        total_correct = correct / Xd.shape[0]
+        total_loss = np.sum(losses) / Xd.shape[0]
+        print("Epoch {2}, Overall loss = {0:.3g} and accuracy of {1:.3g}" \
+              .format(total_loss, total_correct, e + 1))
         if plot_losses:
             plt.plot(losses)
             plt.grid(True)
-            plt.title('Epoch {} Loss'.format(e+1))
+            plt.title('Epoch {} Loss'.format(e + 1))
             plt.xlabel('minibatch number')
             plt.ylabel('minibatch loss')
             plt.show()
-    return total_loss,total_correct
+    return total_loss, total_correct
 
 
 # define model
-def complex_model(X,y,is_training):
+def complex_model(X, y, is_training):
     # Set up variables
-    Wconv1 = tf.get_variable("Wconv1",shape=[7,7,3,32])
-    bconv1 = tf.get_variable("bconv1",shape=[32])
-    Waffine1 = tf.get_variable("Waffine1", shape=[5408,1024])
+    Wconv1 = tf.get_variable("Wconv1", shape=[7, 7, 3, 32])
+    bconv1 = tf.get_variable("bconv1", shape=[32])
+    Waffine1 = tf.get_variable("Waffine1", shape=[5408, 1024])
     baffine1 = tf.get_variable("baffine1", shape=[1024])
-    Waffine2 = tf.get_variable("Waffine2", shape=[1024,10])
+    Waffine2 = tf.get_variable("Waffine2", shape=[1024, 10])
     baffine2 = tf.get_variable("baffine2", shape=[10])
-    
+
     # Define model
-    conv1act = tf.nn.conv2d(X, Wconv1, strides=[1,1,1,1], padding='VALID') + bconv1
+    conv1act = tf.nn.conv2d(X, Wconv1, strides=[1, 1, 1, 1], padding='VALID') + bconv1
     relu1act = tf.nn.relu(conv1act)
     # Using the tf.layers batch norm as its easier
     batchnorm1act = tf.layers.batch_normalization(relu1act, training=is_training)
-    pool1act = tf.nn.max_pool(batchnorm1act, ksize=[1,2,2,1], strides=[1,2,2,1], padding='VALID')
-    flatten1 = tf.reshape(pool1act,[-1,5408])
+    pool1act = tf.nn.max_pool(batchnorm1act, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+    flatten1 = tf.reshape(pool1act, [-1, 5408])
     affine1act = tf.matmul(flatten1, Waffine1) + baffine1
     relu2act = tf.nn.relu(affine1act)
     y_out = tf.matmul(relu2act, Waffine2) + baffine2
-    
+
     return y_out
 
 
 def run_model_cpu():
-    X_train, y_train, X_val, y_val, X_test, y_test = get_CIFAR10_data()    
+    X_train, y_train, X_val, y_val, X_test, y_test = get_CIFAR10_data()
     with tf.Session() as sess:
-        with tf.device("/cpu:0"): #"/cpu:0" or "/gpu:0" 
+        with tf.device("/cpu:0"):  # "/cpu:0" or "/gpu:0"
             sess.run(tf.global_variables_initializer())
             print('Training')
-            run_model(sess,y_out,mean_loss,X_train,y_train,1,64,100,train_step,True)
+            run_model(sess, y_out, mean_loss, X_train, y_train, 1, 64, 100, train_step, True)
             print('Validation')
-            run_model(sess,y_out,mean_loss,X_val,y_val,1,64)
-        
+            run_model(sess, y_out, mean_loss, X_val, y_val, 1, 64)
+
+
 if __name__ == "__main__":
     run_model_cpu()
