e180d39074b4df778455b427117a3c7b3b44ef90
adam-dziedzic <adam.dziedzi@gmail.com>
2018-09-07 19:35:53 -0500

    set gpu

--- cnns/nnlib/pytorch_timeseries/fcnn_keras_1D.py
+++ cnns/nnlib/pytorch_timeseries/fcnn_keras_1D.py
@@ -13,9 +13,15 @@ import os
 import keras
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 from keras.callbacks import ReduceLROnPlateau
 from keras.models import Model
 from keras.utils import np_utils
+
+
+config = tf.ConfigProto(device_count={'GPU': 1, 'CPU': 12})
+sess = tf.Session(config=config)
+keras.backend.set_session(sess)
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print("current working directory: ", dir_path)
@@ -36,13 +42,20 @@ def readucr(filename, data_type):
 
 nb_epochs = 2000
 
-flist = ['Adiac', 'Beef', 'CBF', 'ChlorineConcentration', 'CinC_ECG_torso', 'Coffee', 'Cricket_X', 'Cricket_Y', 'Cricket_Z',
-'DiatomSizeReduction', 'ECGFiveDays', 'FaceAll', 'FaceFour', 'FacesUCR', '50words', 'FISH', 'Gun_Point', 'Haptics',
-'InlineSkate', 'ItalyPowerDemand', 'Lighting2', 'Lighting7', 'MALLAT', 'MedicalImages', 'MoteStrain', 'NonInvasiveFatalECG_Thorax1',
-'NonInvasiveFatalECG_Thorax2', 'OliveOil', 'OSULeaf', 'SonyAIBORobotSurface', 'SonyAIBORobotSurfaceII', 'StarLightCurves', 'SwedishLeaf', 'Symbols',
-'synthetic_control', 'Trace', 'TwoLeadECG', 'Two_Patterns', 'uWaveGestureLibrary_X', 'uWaveGestureLibrary_Y', 'uWaveGestureLibrary_Z', 'wafer', 'WordsSynonyms', 'yoga']
+flist = ['Adiac', 'Beef', 'CBF', 'ChlorineConcentration', 'CinC_ECG_torso',
+         'Coffee', 'Cricket_X', 'Cricket_Y', 'Cricket_Z',
+         'DiatomSizeReduction', 'ECGFiveDays', 'FaceAll', 'FaceFour',
+         'FacesUCR', '50words', 'FISH', 'Gun_Point', 'Haptics',
+         'InlineSkate', 'ItalyPowerDemand', 'Lighting2', 'Lighting7', 'MALLAT',
+         'MedicalImages', 'MoteStrain', 'NonInvasiveFatalECG_Thorax1',
+         'NonInvasiveFatalECG_Thorax2', 'OliveOil', 'OSULeaf',
+         'SonyAIBORobotSurface', 'SonyAIBORobotSurfaceII', 'StarLightCurves',
+         'SwedishLeaf', 'Symbols',
+         'synthetic_control', 'Trace', 'TwoLeadECG', 'Two_Patterns',
+         'uWaveGestureLibrary_X', 'uWaveGestureLibrary_Y',
+         'uWaveGestureLibrary_Z', 'wafer', 'WordsSynonyms', 'yoga']
 
-#flist = ['50words']
+# flist = ['50words']
 for each in flist:
     print("Dataset: ", each)
     fname = each
