3f521d43c66e97ec10e08f4fc0397c9ce724c788
Adam Dziedzic <adam.cajf@gmail.com>
2018-09-28 09:19:30 -0500

    improved the benchmark

--- cnns/nnlib/pytorch_cuda/benchmark.py
+++ cnns/nnlib/pytorch_cuda/benchmark.py
@@ -1,8 +1,8 @@
 import torch
 import time
-from cnns.nnlib.pytorch_cuda.cpp.lltm import LLTM as LLTM_cpp
-from cnns.nnlib.pytorch_cuda.python.lltm import LLTM as LLTM_py
-from cnns.nnlib.pytorch_cuda.cuda.lltm import LLTM as LLTM_cuda
+from cnns.nnlib.pytorch_cuda.cpp.lltm import LLTM as LLTM_CPP
+from cnns.nnlib.pytorch_cuda.python.lltm import LLTM as LLTM_PY
+from cnns.nnlib.pytorch_cuda.cuda.lltm import LLTM as LLTM_CUDA
 
 batch_size = 16
 input_features = 32
@@ -10,9 +10,13 @@ state_size = 128
 
 device = torch.device("cpu")
 
-X = torch.randn(batch_size, input_features)
-h = torch.randn(batch_size, state_size)
-C = torch.randn(batch_size, state_size)
+
+def init_vars(device):
+    X = torch.randn(batch_size, input_features, device=device)
+    h = torch.randn(batch_size, state_size, device=device)
+    C = torch.randn(batch_size, state_size, device=device)
+
+    return X, h, C
 
 
 def test(X, h, C, device=torch.device("cpu")):
@@ -34,35 +38,36 @@ def test(X, h, C, device=torch.device("cpu")):
     return forward, backward
 
 
-rnn = LLTM_py(input_features, state_size, device=device)
-forward, backward = test(X, h, C, device=device)
-print('lltm cpu python: Forward: {:.3f} sec | Backward {:.3f} sec'.format(
-    forward, backward))
+FORWARD_BACK_INFO = "device,{},mode,{},Forward,{:.3f},sec,Backward,{:.3f},sec"
 
-rnn = LLTM_cpp(input_features, state_size, device=device)
+device = torch.device("cpu")
+
+X, h, C = init_vars(device=device)
+rnn = LLTM_PY(input_features, state_size, device=device)
 forward, backward = test(X, h, C, device=device)
-print('lltm cpu cpp: Forward: {:.3f} sec | Backward {:.3f} sec'.format(
-    forward, backward))
+print(FORWARD_BACK_INFO.format(str(device), 'python', forward, backward))
+
+X, h, C = init_vars(device=device)
+rnn = LLTM_CPP(input_features, state_size, device=device)
+forward, backward = test(X, h, C, device=device)
+print(FORWARD_BACK_INFO.format(str(device), 'cpp', forward, backward))
 
 device = torch.device("cuda")
-X = X.to(device)
-h = h.to(device)
-C = C.to(device)
 
-rnn = LLTM_py(input_features, state_size, device=device).to(device)
+X, h, C = init_vars(device=device)
+rnn = LLTM_PY(input_features, state_size, device=device).to(device)
 forward, backward = test(X, h, C, device=device)
-print('lltm gpu python: Forward: {:.3f} sec | Backward {:.3f} sec'.format(
-    forward, backward))
+print(FORWARD_BACK_INFO.format(str(device), 'python', forward, backward))
 
-rnn = LLTM_cpp(input_features, state_size, device=device).to(device)
+X, h, C = init_vars(device=device)
+rnn = LLTM_CPP(input_features, state_size, device=device).to(device)
 forward, backward = test(X, h, C, device=device)
-print('lltm gpu cpp: Forward: {:.3f} sec | Backward {:.3f} sec'.format(
-    forward, backward))
+print(FORWARD_BACK_INFO.format(str(device), 'cpp', forward, backward))
 
-rnn = LLTM_cuda(input_features, state_size, device=device).to(device)
+X, h, C = init_vars(device=device)
+rnn = LLTM_CUDA(input_features, state_size, device=device).to(device)
 forward, backward = test(X, h, C, device=device)
-print('lltm gpu cuda cpp: Forward: {:.3f} sec | Backward {:.3f} sec'.format(
-    forward, backward))
+print(FORWARD_BACK_INFO.format(str(device), 'cuda', forward, backward))
 
 """
 ssh://cc@129.114.108.89:22/home/cc/anaconda3/bin/python -u /home/cc/code/time-series-ml/cnns/nnlib/pytorch_cuda/benchmark.py
