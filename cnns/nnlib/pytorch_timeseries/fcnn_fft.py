7bdbf3eb0c64f71f2eaf7d7ca2388d5c53c54c56
Adam Dziedzic <adam.cajf@gmail.com>
2018-11-02 14:48:33 -0500

    bug set view to x = x.view(-1, 320) because the last batch can be smaller than 64

--- cnns/nnlib/pytorch_timeseries/fcnn_fft.py
+++ cnns/nnlib/pytorch_timeseries/fcnn_fft.py
@@ -551,7 +551,10 @@ class LeNet(nn.Module):
     def forward(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(self.batch_size, 320)
+        # it can't be self.batch_size, 320, because:
+        # the last batch can be smaller then 64:
+        # RuntimeError: shape '[64, 320]' is invalid for input of size 10240
+        x = x.view(-1, 320)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
