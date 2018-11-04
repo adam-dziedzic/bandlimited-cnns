836a88d57caaeac33008e420f809d7e753f910a5
adam-dziedzic <adam.dziedzi@gmail.com>
2018-11-04 16:55:56 -0600

    winograd 2x2 output, 3x3 filter

--- cnns/nnlib/pytorch_layers/conv2D_winograd.py
+++ cnns/nnlib/pytorch_layers/conv2D_winograd.py
@@ -1,14 +1,48 @@
 import sys
+import torch
+from torch import tensor
+
 
 class Winograd(object):
+
     def __init__(self, filter_value=None):
         super(Winograd, self).__init__()
-        if filter_value is None:
+
+        if filter_value is not None:
+            self.filter = filter_value
+        self.B = tensor(
+            [[1.0, 0.0, 0.0, 0.0],
+             [0.0, 1.0, -1.0, 1.0],
+             [-1.0, 1.0, 1.0, 0.0],
+             [0.0, 0.0, 0.0, -1.0]])
+        self.B_T = self.B.transpose(1, 0)
+        self.G = tensor(
+            [[1.0, 0.0, 0.0],
+             [0.5, 0.5, 0.5],
+             [0.5, -0.5, 0.5],
+             [0.0, 0.0, 1.0]])
+        self.G_T = self.G.transpose(1, 0)
+        self.A = tensor([[1.0, 0.0],
+                         [1.0, 1.0],
+                         [1.0, -1.0],
+                         [0.0, -1.0]])
+        self.A_T = self.A.transpose(1, 0)
 
 
-    def
+    def winograd_F_2_3(self, input, filter):
+        """
+        Compute winograd convolution with output of size 2x2 and filter of size
+        3x3.
 
+        :param input: 4x4
+        :param filter: 3x3
+        :return: 2x2
+        """
+        U = torch.matmul(self.G, torch.matmul(filter, self.G_T))
+        V = torch.matmul(self.B_T, torch.matmul(input, self.B))
+        return torch.matmul(self.A_T, torch.matmul(U*V, self.A))
 
 if __name__ == "__main__":
     import doctest
+
     sys.exit(doctest.testmod()[0])
--- cnns/nnlib/pytorch_layers/conv2D_winograd_test.py
+++ cnns/nnlib/pytorch_layers/conv2D_winograd_test.py
@@ -4,12 +4,13 @@ import logging
 import unittest
 import numpy as np
 import torch
-
+from torch import tensor
+from cnns.nnlib.pytorch_layers.conv2D_winograd import Winograd
 
 class TestPyTorchConv1d(unittest.TestCase):
 
     def setUp(self):
-        log_file = "pytorch_conv1D_reuse_map_fft.log"
+        log_file = "pytorch_conv2D_winograd.log"
         is_debug = True
         set_up_logging(log_file=log_file, is_debug=is_debug)
         self.logger = get_logger(name=__name__)
@@ -17,14 +18,20 @@ class TestPyTorchConv1d(unittest.TestCase):
         self.logger.info("Set up test")
 
     def testSimpleWinograd(self):
-        x = np.array([[[1., 2., 3., 1., 4., 5.]]])
-        y = np.array([[[2., 1., 3.]]])
-        b = np.array([0.0])
-        # get the expected results from numpy correlate
-        expected_result = np.correlate(x[0, 0, :], y[0, 0, :], mode="valid")
-
-        conv = Conv1dfftCompressSignalOnly(filter_value=torch.from_numpy(y),
-                                           bias_value=torch.from_numpy(b))
-        result = conv.forward(input=torch.from_numpy(x))
+        x = tensor([[[[1.0, 2.0, 3.0, -1.0],
+                      [3.0, 4.0, 1.0, 2.0],
+                      [1.0, 2.0, 1.0, -2.0],
+                      [2.0, 1.0, -1.0, 2.0]]]])
+        # A single filter.
+        y = tensor([[[[1.0, 2.0, -1.0],
+                      [3.0, 2.0, 1.0],
+                      [4.0, 1.0, -2.0]]]])
+        expect = torch.nn.functional.conv2d(x, y)
+        result = Winograd().winograd_F_2_3(x, y)
         np.testing.assert_array_almost_equal(
-            result, np.array([[expected_result]]))
+            x=expect, y=result,
+            err_msg="The expected array x and computed y are not almost equal.")
+
+
+if __name__ == '__main__':
+    unittest.main()
