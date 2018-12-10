113f9f33a1c5705a1f3d339914ddca0f8ed327d4
adam-dziedzic <adam.dziedzi@gmail.com>
2018-12-09 19:59:51 -0600

    update the script to reinstall cuda

--- cnns/nnlib/pytorch_cuda/complex_mul_cuda/install.sh
+++ cnns/nnlib/pytorch_cuda/complex_mul_cuda/install.sh
@@ -2,5 +2,7 @@
 rm -rf complex_mul_cuda.egg-info/
 rm -rf build/
 rm -rf dist/
-rm -rf /local/ady/anaconda3/lib/python3.6/site-packages/complex_mul_cuda-0.0.0-py3.6-linux-x86_64.egg
+rm -rf /local/${USER}/anaconda3/lib/python3.6/site-packages/complex_mul_cuda-0.0.0-py3.6-linux-x86_64.egg
+rm -rf ~/anaconda3/lib/python3.6/site-packages/complex_mul_cuda-0.0.0-py3.6-linux-x86_64.egg
 ~/anaconda3/bin/python3.6 setup.py install
+# python setup.py install
