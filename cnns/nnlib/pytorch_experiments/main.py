a6db654bcf9d8e46331137f7e3b64b95f88f6bb3
adam-dziedzic <adam.dziedzi@gmail.com>
2019-01-16 10:01:45 -0800

    get model

--- cnns/nnlib/pytorch_experiments/main.py
+++ cnns/nnlib/pytorch_experiments/main.py
@@ -21,33 +21,26 @@ from torch.optim.lr_scheduler import \
 from torch.optim.lr_scheduler import MultiStepLR
 
 from cnns.nnlib.pytorch_layers.AdamFloat16 import AdamFloat16
-from cnns.nnlib.pytorch_architecture.model_utils import getModelPyTorch
-from cnns.nnlib.pytorch_architecture.le_net import LeNet
-from cnns.nnlib.pytorch_architecture.resnet2d import resnet18
-from cnns.nnlib.pytorch_architecture.densenet import densenet_cifar
-from cnns.nnlib.pytorch_architecture.fcnn import FCNNPytorch
-from cnns.nnlib.utils.general_utils import ConvType
-from cnns.nnlib.utils.general_utils import ConvExecType
-from cnns.nnlib.utils.general_utils import CompressType
+# from cnns.nnlib.pytorch_architecture.model_utils import getModelPyTorch
 from cnns.nnlib.utils.general_utils import OptimizerType
 from cnns.nnlib.utils.general_utils import SchedulerType
 from cnns.nnlib.utils.general_utils import LossType
 from cnns.nnlib.utils.general_utils import LossReduction
-from cnns.nnlib.utils.general_utils import NetworkType
-from cnns.nnlib.utils.general_utils import MemoryType
 from cnns.nnlib.utils.general_utils import TensorType
-from cnns.nnlib.utils.general_utils import StrideType
 from cnns.nnlib.utils.general_utils import PrecisionType
-from cnns.nnlib.utils.general_utils import Bool
 from cnns.nnlib.utils.general_utils import additional_log_file
 from cnns.nnlib.utils.general_utils import mem_log_file
 from cnns.nnlib.utils.general_utils import get_log_time
 from cnns.nnlib.datasets.mnist import get_mnist
 from cnns.nnlib.datasets.cifar import get_cifar
 from cnns.nnlib.datasets.ucr.ucr import get_ucr
-from cnns.nnlib.utils.arguments import Arguments
 from cnns.nnlib.utils.exec_args import get_args
 from cnns.nnlib.pytorch_experiments.utils.progress_bar import progress_bar
+from cnns.nnlib.pytorch_architecture.le_net import LeNet
+from cnns.nnlib.pytorch_architecture.resnet2d import resnet18
+from cnns.nnlib.pytorch_architecture.densenet import densenet_cifar
+from cnns.nnlib.pytorch_architecture.fcnn import FCNNPytorch
+from cnns.nnlib.utils.general_utils import NetworkType
 
 # from memory_profiler import profile
 
@@ -106,6 +99,35 @@ if torch.cuda.is_available() and args.use_cuda:
     # torch.set_default_tensor_type('torch.cuda.FloatTensor')
 else:
     device = torch.device("cpu")
+
+
+def getModelPyTorch(args):
+    """
+    Get the PyTorch version of the FCNN model.
+    :param input_size: the length (width) of the time series.
+    :param num_classes: number of output classes.
+    :param in_channels: number of channels in the input data for a convolution.
+    :param out_channels: number of channels in the output of a convolution.
+    :param dtype: global - the type of torch data/weights.
+    :param flat_size: the size of the flat vector after the conv layers.
+    :return: the model.
+    """
+    network_type = args.network_type
+    if network_type is NetworkType.LE_NET:
+        return LeNet(args=args)
+    elif network_type is NetworkType.FCNN_SMALL or (
+            network_type is NetworkType.FCNN_STANDARD):
+        if network_type is NetworkType.FCNN_SMALL:
+            args.out_channels = [1, 1, 1]
+        elif network_type is NetworkType.FCNN_STANDARD:
+            args.out_channels = [128, 256, 128]
+        return FCNNPytorch(args=args)
+    elif network_type == NetworkType.ResNet18:
+        return resnet18(args=args)
+    elif network_type == NetworkType.DenseNetCifar:
+        return densenet_cifar(args=args)
+    else:
+        raise Exception("Unknown network_type: ", network_type)
 
 
 def readucr(filename, data_type):
