cd5cd23ef78cb1657f3d5459c68b4b03e982f425
adam-dziedzic <adam.dziedzi@gmail.com>
2019-01-18 21:24:46 -0600

    mem test

--- cnns/experiments/nvidia-capture-run.sh
+++ cnns/experiments/nvidia-capture-run.sh
@@ -2,7 +2,7 @@
 
 for compress_rate in 0 25 50 75; do
     for data_type in "fp16" "fp32"; do
-        bash nvidia-capture.sh ${compress_rate} 1 32 ${data_type}
+        bash nvidia-capture.sh ${compress_rate} 1 32 ${data_type} "TRUE"
     done;
 done;
 
