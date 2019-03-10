f615282a1bf17f3a07bcb3b1bc9da6ef315ccfdb
adam-dziedzic <adam.dziedzi@gmail.com>
2019-03-09 22:59:07 -0600

    adversarial

--- cnns/nnlib/pytorch_experiments/robustness/pytorch_resnet18_imagenet_attack.py
+++ cnns/nnlib/pytorch_experiments/robustness/pytorch_resnet18_imagenet_attack.py
@@ -26,35 +26,36 @@ images = images / 255
 resnet18 = models.resnet18(pretrained=True).cuda().eval()  # for CPU, remove cuda()
 mean = np.array([0.485, 0.456, 0.406]).reshape((3, 1, 1))
 std = np.array([0.229, 0.224, 0.225]).reshape((3, 1, 1))
-model = foolbox.models.PyTorchModel(resnet18, bounds=(0, 1), num_classes=1000,
+foolbox_model = foolbox.models.PyTorchModel(resnet18, bounds=(0, 1), num_classes=1000,
                                     preprocessing=(mean, std))
 
+class empty_attack(foolbox.attacks.base.Attack):
 
-def empty_attack(image, _):
-    return image
+    def __call__(self, input_or_adv, label=None, unpack=True, **kwargs):
+        return input_or_adv
 
 
 # attack = foolbox.attacks.FGSM(model)
 attack = empty_attack
 
-print("full_attack, attack, correct (%)")
+print("attack_name, correct (%)")
 attacks = [# empty_attack,
-           foolbox.attacks.SinglePixelAttack(model),
-           foolbox.attacks.FGSM(model),
-           foolbox.attacks.GradientAttack(model),
-           foolbox.attacks.LinfinityBasicIterativeAttack(
-               model, distance=foolbox.distances.MeanSquaredDistance),
+           # foolbox.attacks.SinglePixelAttack(model),
+           foolbox.attacks.FGSM(foolbox_model),
+           # foolbox.attacks.GradientAttack(model),
+           # foolbox.attacks.LinfinityBasicIterativeAttack(
+           # model, distance=foolbox.distances.MeanSquaredDistance),
            ]
+
+
 for attack in attacks:
     correct = 0
     counter = 0
     for image, label in zip(images, labels):
-        attack_name 
-        image = attack(image, label)
-        predictions = model.predictions(image)
+        image = attack(image, label, epsilons=2, max_epsilon=0.001)
+        predictions = foolbox_model.predictions(image)
         # print(np.argmax(predictions), label)
         if np.argmax(predictions) == label:
             correct += 1
         counter += 1
-    print(str(attack), ",", , ",",
-          correct / counter)
+    print(attack.name(), ",", correct / counter)
