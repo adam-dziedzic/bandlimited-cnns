cca4cc4c248fbd1b191420c8c5cfe71efa4fd338
adam-dziedzic <adam.dziedzi@gmail.com>
2019-03-20 17:39:26 -0500

    tests

--- cnns/nnlib/pytorch_experiments/robustness/show_adversarial_fft_pytorch.py
+++ cnns/nnlib/pytorch_experiments/robustness/show_adversarial_fft_pytorch.py
@@ -31,25 +31,28 @@ def softmax(x):
 fft_type = "magnitude"
 save_out = False
 
-def to_fft(x):
+
+def to_fft(x, is_log=True):
     x = torch.from_numpy(x)
     # x = torch.tensor(x)
-    x = x.permute(2, 0, 1)  # move channel as the first dimension
+    # x = x.permute(2, 0, 1)  # move channel as the first dimension
     xfft = torch.rfft(x, onesided=False, signal_ndim=2)
     if fft_type == "magnitude":
-        return to_fft_magnitude(xfft)
+        return to_fft_magnitude(xfft, is_log)
     elif fft_type == "phase":
         return to_fft_phase(xfft)
     else:
         raise Exception(f"Unknown type of fft processing: {fft_type}")
 
 
-def to_fft_magnitude(xfft):
+def to_fft_magnitude(xfft, is_log=True):
     _, xfft_squared = get_full_energy(xfft)
     xfft_abs = torch.sqrt(xfft_squared)
     # xfft_abs = xfft_abs.sum(dim=0)
-    # return np.log(xfft_abs.numpy())
-    return xfft_abs.numpy()
+    if is_log:
+        return np.log(xfft_abs.numpy())
+    else:
+        return xfft_abs.numpy()
 
 
 def to_fft_phase(xfft):
@@ -161,168 +164,179 @@ def print_color_map(x, fig=None, ax=None):
                         ha='center', va='center')
 
 
-# get source image and label
-# image, label = foolbox.utils.imagenet_example()
-idx = 0
-image, label = images[idx], labels[idx]
-
-# apply attack on source image
-# ::-1 reverses the color channels, because Keras ResNet50 expects BGR instead of RGB
-
-attack = foolbox.attacks.FGSM(fmodel)
-adversarial = attack(image, label)
-
-print("original label: id:", label, ", class: ",
-      imagenet_from_class_idx_to_label[label])
-
-# predictions_original = kmodel.predict(image[np.newaxis, :, :, ::-1])
-predictions_original, _ = fmodel.predictions_and_gradient(image=image,
-                                                          label=label)
-# predictions_original = znormalize(predictions_original)
-predictions_original = softmax(predictions_original)
-original_prediction = imagenet_from_class_idx_to_label[
-    np.argmax(predictions_original)]
-print("model original prediction: ", original_prediction)
-original_confidence = np.max(predictions_original)
-sum_predictions = np.sum(predictions_original)
-# print("sum predictions: ", sum_predictions)
-# print("predictions_original: ", predictions_original)
-
-# predictions_adversarial = kmodel.predict(adversarial[np.newaxis, ...])
-predictions_adversarial, _ = fmodel.predictions_and_gradient(
-    image=adversarial, label=label)
-# predictions_adversarial = znormalize(predictions_adversarial)
-predictions_adversarial = softmax(predictions_adversarial)
-adversarial_prediction = imagenet_from_class_idx_to_label[
-    np.argmax(predictions_adversarial)]
-adversarial_confidence = np.max(predictions_adversarial)
-
-print("model adversarial prediciton: ", adversarial_prediction)
-
-# attack = foolbox.attacks.MultiplePixelsAttack(fmodel)
-# adversarial = attack(image, label, num_pixels=100)
-
-# attack = foolbox.attacks.AdditiveUniformNoiseAttack(fmodel)
-# adversarial = attack(image[:, :, ::-1], label, epsilons=[0.4])
-
-# adversarial = adversarial[:, :, ::-1].copy()  # from BGR to RGB
-# print("adversarial: ", adversarial)
-# if the attack fails, adversarial will be None and a warning will be printed
-
-if adversarial is None:
-    raise Exception('foolbox did not find an adversarial example')
-
 channels_nr = 1
 channels = [x for x in range(channels_nr)]
-rows = 1 + 2 * len(channels)
+attacks = [foolbox.attacks.FGSM(fmodel),
+           foolbox.attacks.AdditiveUniformNoiseAttack(fmodel)]
+# attacks = [foolbox.attacks.FGSM(fmodel)]
+rows = len(attacks) * (1 + 2 * len(channels))
 cols = 3
 
-fig = plt.figure(figsize=(15, 15))
-plt.subplot(rows, cols, 1)
-plt.title('Original\nlabel: ' + str(
-    original_prediction.replace(",",
-                                "\n") + "\nconfidence: " + str(
-        np.around(original_confidence, decimals=decimals))))
-image = np.moveaxis(image, 0, -1)
-plt.imshow(image)  # move channels to last dimension
-# plt.imshow(image / 255)  # division by 255 to convert [0, 255] to [0, 1]
-plt.axis('off')
+fig = plt.figure(figsize=(30, 30))
 
-plt.subplot(rows, cols, 2)
-plt.title('Adversarial\nlabel: ' + str(
-    adversarial_prediction.replace(",",
-                                   "\n") + "\nconfidence: " + str(
-        np.around(adversarial_confidence, decimals=decimals))))
-adversarial = np.moveaxis(adversarial, 0, -1)
-plt.imshow(adversarial)
-plt.axis('off')
+i = 1
 
-plt.subplot(rows, cols, 3)
-plt.title('Difference')
-difference = adversarial - image
-# https://www.statisticshowto.datasciencecentral.com/normalized/
-difference = (difference - difference.min()) / (
-        difference.max() - difference.min())
-# plt.imshow(difference / abs(difference).max() * 0.2 + 0.5)
-plt.imshow(difference)
-plt.axis('off')
+for attack in attacks:
 
-i = 4
-for fft_type in ["phase", "magnitude"]:
-    for channel in channels:
-        ax = plt.subplot(rows, cols, i)
-        i += 1
-        # plt.title('Original\nfft-ed')
-        original_fft = to_fft(image)
-        original_fft = original_fft[channel]
-        # torch.set_printoptions(profile='full')
-        # print("original_fft size: ", original_fft.shape)
-        # options = np.get_printoptions()
-        # np.set_printoptions(threshold=np.inf)
-        # torch.set_printoptions(profile='default')
-        # print("original_fft: ", original_fft)
+    # get source image and label
+    # image, label = foolbox.utils.imagenet_example()
+    idx = 1
+    image, label = images[idx], labels[idx]
 
-        # save to file
-        if save_out:
-            dir_path = os.path.dirname(os.path.realpath(__file__))
-            output_path = os.path.join(dir_path, "original_fft.csv")
-            print("output path: ", output_path)
-            np.save(output_path, original_fft)
+    # apply attack on source image
+    # ::-1 reverses the color channels, because Keras ResNet50 expects BGR instead of RGB
 
-        # go back to the original print size
-        # np.set_printoptions(threshold=options['threshold'])
-        original_fft = original_fft[:lim_y, :lim_x]
-        print_color_map(original_fft, fig, ax)
+    print("original label: id:", label, ", class: ",
+          imagenet_from_class_idx_to_label[label])
 
-        # plt.axis('off')
-        plt.ylabel("fft-ed channel " + str(channel) + ": " + fft_type)
+    # predictions_original = kmodel.predict(image[np.newaxis, :, :, ::-1])
+    predictions_original, _ = fmodel.predictions_and_gradient(image=image,
+                                                              label=label)
+    # predictions_original = znormalize(predictions_original)
+    predictions_original = softmax(predictions_original)
+    original_prediction = imagenet_from_class_idx_to_label[
+        np.argmax(predictions_original)]
+    print("model original prediction: ", original_prediction)
+    original_confidence = np.max(predictions_original)
+    sum_predictions = np.sum(predictions_original)
+    # print("sum predictions: ", sum_predictions)
+    # print("predictions_original: ", predictions_original)
 
-        ax = plt.subplot(rows, cols, i)
-        i += 1
-        # plt.title('Adversarial\nfft-ed')
-        adversarial_fft = to_fft(adversarial)
-        adversarial_fft = adversarial_fft[channel]
+    plt.subplot(rows, cols, i)
+    i += 1
+    plt.title('Original\nlabel: ' + str(
+        original_prediction.replace(",",
+                                    "\n") + "\nconfidence: " + str(
+            np.around(original_confidence, decimals=decimals))))
+    plt.imshow(np.moveaxis(image, 0, -1))  # move channels to last dimension
+    # plt.imshow(image / 255)  # division by 255 to convert [0, 255] to [0, 1]
+    plt.axis('off')
 
-        if save_out:
-            output_path = os.path.join(dir_path, "adversarial_fft.csv")
-            print("output path: ", output_path)
-            np.save(output_path, adversarial_fft)
+    plt.subplot(rows, cols, i)
+    i += 1
 
-        adversarial_fft = adversarial_fft[:lim_y, :lim_x]
+    adversarial = attack(image, label)
+    # predictions_adversarial = kmodel.predict(adversarial[np.newaxis, ...])
+    predictions_adversarial, _ = fmodel.predictions_and_gradient(
+        image=adversarial, label=label)
+    # predictions_adversarial = znormalize(predictions_adversarial)
+    predictions_adversarial = softmax(predictions_adversarial)
+    adversarial_prediction = imagenet_from_class_idx_to_label[
+        np.argmax(predictions_adversarial)]
+    adversarial_confidence = np.max(predictions_adversarial)
 
-        print_color_map(adversarial_fft, fig, ax)
+    print("model adversarial prediciton: ", adversarial_prediction)
 
-        # plt.axis('off')
+    # attack = foolbox.attacks.MultiplePixelsAttack(fmodel)
+    # adversarial = attack(image, label, num_pixels=100)
 
-        # plt.subplot(2, 3, 6)
-        # plt.title('FFT Difference')
-        # difference_fft = adversarial_fft - original_fft
-        # final_difference = difference_fft / abs(difference_fft).max() * 0.2 + 0.5
-        # plt.imshow(final_difference)
-        # heatmap_legend = plt.pcolor(final_difference)
-        # plt.colorbar(heatmap_legend)
-        # plt.axis('off')
+    # attack = foolbox.attacks.AdditiveUniformNoiseAttack(fmodel)
+    # adversarial = attack(image[:, :, ::-1], label, epsilons=[0.4])
 
-        ax = plt.subplot(rows, cols, i)
-        i += 1
-        # plt.title('Difference\nfft-ed')
-        # diff_type = "source"
-        diff_type = "fft"
+    # adversarial = adversarial[:, :, ::-1].copy()  # from BGR to RGB
+    # print("adversarial: ", adversarial)
+    # if the attack fails, adversarial will be None and a warning will be printed
 
-        if diff_type == "source":
-            difference = np.abs((image / 255) - (adversarial / 255))
-            difference = adversarial - image
-            difference_fft = difference_fft[channel]
-            difference_fft = difference_fft[:lim_y, :lim_x]
-        elif diff_type == "fft":
-            # difference = (adversarial_fft - original_fft)[..., np.newaxis]
-            # difference_fft = to_fft(difference)
-            difference_fft = adversarial_fft - original_fft
-        else:
-            raise Exception(f"Unknown diff_type: {diff_type}")
+    if adversarial is None:
+        raise Exception('foolbox did not find an adversarial example')
 
-        print_color_map(difference_fft, fig, ax)
-        # plt.axis('off')
+    plt.title('Adversarial ' + attack.name() + '\nlabel: ' + str(
+        adversarial_prediction.replace(",",
+                                       "\n") + "\nconfidence: " + str(
+            np.around(adversarial_confidence, decimals=decimals))))
+    plt.imshow(np.moveaxis(adversarial, 0, -1))
+    plt.axis('off')
+
+    plt.subplot(rows, cols, i)
+    i += 1
+    plt.title('Difference')
+    difference = adversarial - image
+    print("difference:\n", difference)
+    # https://www.statisticshowto.datasciencecentral.com/normalized/
+    # difference = (difference - difference.min()) / (
+    #         difference.max() - difference.min())
+    # plt.imshow(difference / abs(difference).max() * 0.2 + 0.5)
+    plt.imshow(np.moveaxis(difference, 0, -1))
+    plt.axis('off')
+
+    for fft_type in ["phase", "magnitude"]:
+        for channel in channels:
+            ax = plt.subplot(rows, cols, i)
+            i += 1
+            # plt.title('Original\nfft-ed')
+            original_fft = to_fft(image)
+            original_fft = original_fft[channel, ...]
+            # torch.set_printoptions(profile='full')
+            # print("original_fft size: ", original_fft.shape)
+            # options = np.get_printoptions()
+            # np.set_printoptions(threshold=np.inf)
+            # torch.set_printoptions(profile='default')
+
+            # save to file
+            if save_out:
+                dir_path = os.path.dirname(os.path.realpath(__file__))
+                output_path = os.path.join(dir_path, "original_fft.csv")
+                print("output path: ", output_path)
+                np.save(output_path, original_fft)
+
+            # go back to the original print size
+            # np.set_printoptions(threshold=options['threshold'])
+            original_fft = original_fft[:lim_y, :lim_x]
+            # print("original_fft:\n", original_fft)
+            print_color_map(original_fft, fig, ax)
+
+            # plt.axis('off')
+            plt.ylabel("fft-ed channel " + str(channel) + ":\n" + fft_type)
+
+            ax = plt.subplot(rows, cols, i)
+            i += 1
+            # plt.title('Adversarial\nfft-ed')
+            adversarial_fft = to_fft(adversarial)
+            adversarial_fft = adversarial_fft[channel]
+
+            if save_out:
+                output_path = os.path.join(dir_path, "adversarial_fft.csv")
+                print("output path: ", output_path)
+                np.save(output_path, adversarial_fft)
+
+            adversarial_fft = adversarial_fft[:lim_y, :lim_x]
+            # print("adversarial fft:\n", adversarial_fft)
+
+            print_color_map(adversarial_fft, fig, ax)
+
+            # plt.axis('off')
+
+            # plt.subplot(2, 3, 6)
+            # plt.title('FFT Difference')
+            # difference_fft = adversarial_fft - original_fft
+            # final_difference = difference_fft / abs(difference_fft).max() * 0.2 + 0.5
+            # plt.imshow(final_difference)
+            # heatmap_legend = plt.pcolor(final_difference)
+            # plt.colorbar(heatmap_legend)
+            # plt.axis('off')
+
+            ax = plt.subplot(rows, cols, i)
+            i += 1
+            # plt.title('Difference\nfft-ed')
+            # diff_type = "source"
+            diff_type = "source"
+
+            if diff_type == "source":
+                difference = np.abs((image / 255) - (adversarial / 255))
+                difference_fft = to_fft(difference, is_log=False)
+                difference_fft = difference_fft[channel]
+                difference_fft = difference_fft[:lim_y, :lim_x]
+            elif diff_type == "fft":
+                # difference = (adversarial_fft - original_fft)[..., np.newaxis]
+                # difference_fft = to_fft(difference)
+                difference_fft = adversarial_fft - original_fft
+            else:
+                raise Exception(f"Unknown diff_type: {diff_type}")
+
+            print_color_map(difference_fft, fig, ax)
+            # plt.axis('off')
+
+    plt.subplots_adjust(hspace=0.6)
 
 format = 'pdf'
 file_name = "images/" + attack.name() + "-channel-" + str(
