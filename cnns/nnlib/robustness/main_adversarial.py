fe45d47c5b56472337b03e8d6bfbe655a7bc2804
adam-dziedzic <adam.dziedzi@gmail.com>
2019-05-20 23:23:04 -0500

    for foolbox

--- cnns/nnlib/robustness/main_adversarial.py
+++ cnns/nnlib/robustness/main_adversarial.py
@@ -373,7 +373,7 @@ def run(args):
             result = Object()
             true_class_id = args.true_class_id
             true_label = args.true_label
-            predictions = fmodel.predictions(
+            predictions, _ = fmodel.predictions_and_gradient(
                 image=image, label=true_class_id)
             predictions = softmax(predictions)
             predicted_class_id = np.argmax(predictions)
