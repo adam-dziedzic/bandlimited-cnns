617d81f1637525903dc92a0bdf506ea6dcf08628
adam-dziedzic <adam.dziedzi@gmail.com>
2019-07-16 16:41:46 -0500

    corrected fcnn - take correct number of output channels

---
+++ cnns/graphs/wifi_nlos_los/wifi-nlos.py
@@ -0,0 +1,123 @@
+import matplotlib
+
+# matplotlib.use('TkAgg')
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+import matplotlib.pyplot as plt
+import csv
+import os
+
+print(matplotlib.get_backend())
+
+# plt.interactive(True)
+# http://ksrowell.com/blog-visualizing-data/2012/02/02/optimal-colors-for-graphs/
+MY_BLUE = (56, 106, 177)
+MY_RED = (204, 37, 41)
+MY_ORANGE = (218, 124, 48)
+MY_GREEN = (62, 150, 81)
+MY_BLACK = (83, 81, 84)
+
+
+def get_color(COLOR_TUPLE_255):
+    return [x / 255 for x in COLOR_TUPLE_255]
+
+
+# fontsize=20
+fontsize = 30
+legend_size = 26
+font = {'size': fontsize}
+matplotlib.rc('font', **font)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print("dir path: ", dir_path)
+
+GPU_MEM_SIZE = 16280
+
+
+def read_columns(dataset, columns=5):
+    file_name = dir_path + "/" + dataset + ".csv"
+    with open(file_name) as csvfile:
+        data = csv.reader(csvfile, delimiter=",", quotechar='|')
+        cols = []
+        for column in range(columns):
+            cols.append([])
+
+        for i, row in enumerate(data):
+            if i > 0:  # skip header
+                for column in range(columns):
+                    cols[column].append(float(row[column]))
+    return cols
+
+
+ylabel = "ylabel"
+title = "title"
+legend_pos = "center_pos"
+bbox = "bbox"
+file_name = "file_name"
+
+energy = {ylabel: "Energy (dB)",
+        file_name: "wifi_energy",
+        title: "accuracy",
+        legend_pos: "upper left",
+        bbox: (0.0, 0.1)}
+
+labels = ["", "0 Wi-Fi", "1 Wi-Fi", "2 Wi-Fis"]
+ncols = [3, 3]
+columns = 4
+
+colors = [get_color(color) for color in
+          ["", MY_GREEN, MY_BLUE, MY_ORANGE, MY_RED, MY_BLACK]]
+markers = ["+", "o", "v", "s", "D", "^"]
+linestyles = ["", "-", "--", ":"]
+
+datasets = [energy]
+
+# width = 12
+# height = 5
+# lw = 3
+
+width = 15
+height = 7
+lw = 4
+
+fig = plt.figure(figsize=(width, len(datasets) * height))
+
+for j, dataset in enumerate(datasets):
+    plt.subplot(len(datasets), 1, j + 1)
+    print("dataset: ", dataset)
+    cols = read_columns(dataset[file_name], columns=columns)
+
+    print("col 0: ", cols[0])
+    print("col 1: ", cols[1])
+
+    for i in range(columns):
+        if i > 0:  # skip first column with the epoch number
+            plt.plot(cols[0], cols[i], label=f"{labels[i]}", lw=lw,
+                     color=colors[i], linestyle=linestyles[i])
+
+    plt.grid()
+    plt.legend(loc=dataset[legend_pos], ncol=ncols[j], frameon=False,
+               prop={'size': legend_size},
+               # bbox_to_anchor=dataset[bbox]
+               )
+    plt.xlabel('Sample number')
+    # plt.title(titles[j], fontsize=16)
+    plt.ylabel(dataset[ylabel])
+    plt.ylim(-40, -15)
+    plt.xlim(0, 2048)
+
+# plt.gcf().autofmt_xdate()
+# plt.xticks(rotation=0)
+# plt.interactive(False)
+# plt.imshow()
+plt.subplots_adjust(hspace=0.3)
+format = "pdf"  # "pdf" or "png"
+destination = dir_path + "/" + "wifi-energy." + format
+print("destination: ", destination)
+fig.savefig(destination,
+            bbox_inches='tight',
+            # transparent=True
+            )
+# plt.show(block=False)
+# plt.interactive(False)
+plt.close()
---
+++ cnns/graphs/wifi_nlos_los/wifi_los_graph.py
@@ -0,0 +1,96 @@
+import matplotlib
+
+matplotlib.rcParams['pdf.fonttype'] = 42
+matplotlib.rcParams['ps.fonttype'] = 42
+import matplotlib.pyplot as plt
+import os
+import numpy as np
+
+print(matplotlib.get_backend())
+
+# plt.interactive(True)
+# http://ksrowell.com/blog-visualizing-data/2012/02/02/optimal-colors-for-graphs/
+MY_BLUE = (56, 106, 177)
+MY_RED = (204, 37, 41)
+MY_ORANGE = (218, 124, 48)
+MY_GREEN = (62, 150, 81)
+MY_BLACK = (83, 81, 84)
+
+
+def get_color(COLOR_TUPLE_255):
+    return [x / 255 for x in COLOR_TUPLE_255]
+
+
+# fontsize=20
+size=18
+fontsize = size
+legend_size = size
+label_size = 12
+
+font = {'size': fontsize}
+matplotlib.rc('font', **font)
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+print("dir path: ", dir_path)
+
+labels = ['ED:LOS', 'ED:NLOS', 'AC:LOS', 'AC:NLOS', 'ML:LOS', 'ML:NLOS']
+v6F = [98.0, 84.0, 99.0, 95.0, 99.8, 99.7]
+v10F = [96.0, 76.0, 98.3, 94.0, 99.4, 99.3]
+v15F = [84.0, 71.0, 98.0, 91.0, 99.2, 99.1]
+
+x = np.arange(len(labels))  # the label locations
+width = 0.3  # the width of the bars
+
+fig_width = 35 * width
+fig_height = 20 * width
+lw = 4
+
+# plt.figure(figsize=(width, height))
+
+fig, ax = plt.subplots(figsize=(fig_width, fig_height))
+rects1 = ax.bar(x - width, v6F, width, label='6F', color=get_color(MY_BLUE))
+rects2 = ax.bar(x, v10F, width, label='10F', color=get_color(MY_RED), hatch='/')
+rects3 = ax.bar(x + width, v10F, width, label='15F',
+                color=get_color(MY_GREEN), hatch='*')
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Successful Detection (%)')
+ax.set_ylim(0, 118)
+ax.set_xlabel('Types of CSAT approach')
+ax.set_xticks(x)
+ax.set_xticklabels(labels)
+ax.legend()
+
+
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 3),  # 3 points vertical offset
+                    textcoords="offset points",
+                    ha='center', va='bottom', fontsize=label_size)
+
+
+autolabel(rects1)
+autolabel(rects2)
+autolabel(rects3)
+
+plt.legend(loc="upper center", ncol=3, frameon=False,
+               prop={'size': legend_size},
+               # bbox_to_anchor=dataset[bbox]
+               )
+
+fig.tight_layout()
+format = "pdf"  # "pdf" or "png"
+destination = dir_path + "/" + "wifi-n-los2." + format
+print("destination: ", destination)
+fig.savefig(destination,
+            bbox_inches='tight',
+            # transparent=True
+            )
+# plt.show(block=False)
+# plt.interactive(False)
+# plt.show()
+plt.close()
