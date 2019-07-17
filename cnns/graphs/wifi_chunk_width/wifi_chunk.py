79a4855aa459df9e991962c67ffa401eb84a50ee
adam-dziedzic <adam.dziedzi@gmail.com>
2019-07-17 09:54:35 -0500

    chunk width graph

--- cnns/graphs/wifi_chunk_width/wifi_chunk.py
+++ cnns/graphs/wifi_chunk_width/wifi_chunk.py
@@ -9,7 +9,7 @@ MY_ORANGE = (218, 124, 48)
 MY_GREEN = (62, 150, 81)
 MY_BLACK = (83, 81, 84)
 
-legend_size = 16
+legend_size = 20
 legend_position = 'best'
 frameon = False
 bbox_to_anchor = (0, -0.1)
@@ -30,12 +30,27 @@ fig = plt.figure(figsize=(8, 6))
 # plt.title("Model accuracy for a given chunk size", fontsize=title_size)
 static_x = (
     1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048)
-static_y = (
+classes_2 = (
     85.01, 87.15, 91.19, 94.92, 97.01, 98.09, 98.92, 99.15, 99.21, 99.28, 99.42,
-99.60)
+    99.60)
+classes_3 = (32.46,
+             53.71646407,
+             65.50106259,
+             63.44205055,
+             72.93496023,
+             74.83560951,
+             80.26110958,
+             87.90859934,
+             96.59671708,
+             99.63,
+             99.95,
+             99.98705502,
+             )
 
-plt.plot(static_x, static_y, label='static compression', lw=2, marker='o',
+plt.plot(static_x, classes_2, label='2 classes', lw=3, marker='o',
          color=get_color(MY_BLUE))
+plt.plot(static_x, classes_3, label='3 classes', lw=3, marker='v',
+         color=get_color(MY_RED))
 # plt.plot(mix_x, mix_y, label='energy first + static rest', lw=2, marker='v',
 #          color=get_color(MY_ORANGE))
 # plt.plot(energy_x, energy_y, label='energy based compression', lw=2, marker='s',
@@ -43,10 +58,17 @@ plt.plot(static_x, static_y, label='static compression', lw=2, marker='o',
 
 plt.xlabel('Chunk size')
 plt.ylabel('Test accuracy (%)')
+plt.xscale('log', basex=2)
+plt.xlim(1, 2048)
+plt.ylim(30, 100)
 plt.grid()
-# plt.legend(loc=legend_position, frameon=frameon, prop={'size': legend_size},
-#            bbox_to_anchor=bbox_to_anchor)
+plt.legend(loc=legend_position,
+           frameon=frameon,
+           prop={'size': legend_size},
+           # bbox_to_anchor=bbox_to_anchor
+           )
 
 plt.show()
-fig.savefig(dir_path + "/" + "test-accuracy-chunk-size.pdf",
+fig.savefig(dir_path + "/" + "test-accuracy-chunk-size2.pdf",
             bbox_inches='tight')
+plt.close()
--- cnns/nnlib/datasets/sathya/data_preparation_many_classes.py
+++ cnns/nnlib/datasets/sathya/data_preparation_many_classes.py
@@ -12,148 +12,152 @@ print("current working directory: ", os.getcwd())
 type = ""
 # sample_size: 1000, 500, 250, 32, 64
 # sample_size = 192  # 500 for small data # how many values in a single sample collected
-sample_size = 64
-train_rate = 0.5  # rate of training data, test data rate is 1 - train_rate
-outlier_std_count = 10
-# class_counter = 5
-# class_counter = 6
-# prefix="test_one_wifi_"
-# prefix="wifi6_data/"
-# suffix="_wifi_165"
+# sample_size = 2048
 
-class_counter = 3
-prefix = "wifi0-1-2/Test_165_"
-suffix = "_Wi-Fi_28"
-datasets = []
-min_len = sys.maxsize  # get the minimum length of dataset for each class
+# for sample_size in [2**x for x in range(12, 0, -1)]:
+for sample_size in [1]:
+    print("sample size: ", str(sample_size))
+    train_rate = 0.5  # rate of training data, test data rate is 1 - train_rate
+    outlier_std_count = 10
+    # class_counter = 5
+    # class_counter = 6
+    # prefix="test_one_wifi_"
+    # prefix="wifi6_data/"
+    # suffix="_wifi_165"
 
-for counter in range(0, class_counter, 1):
-    csv_path = prefix + str(counter) + suffix + type + ".txt"
+    class_counter = 3
+    prefix = "wifi0-1-2/Test_165_"
+    suffix = "_Wi-Fi_28"
+    datasets = []
+    min_len = sys.maxsize  # get the minimum length of dataset for each class
 
-    # data1 = pd.read_csv(csv_path1, header=None)
-    # # print("data1 values: ", data1.values)
-    # data1 = np.array(data1.values).squeeze()
+    for counter in range(0, class_counter, 1):
+        csv_path = prefix + str(counter) + suffix + type + ".txt"
 
-    dataset = np.genfromtxt(csv_path, delimiter="\n")
-    dataset = np.delete(dataset, np.where(
-        (dataset == float("-inf")) or (dataset == float("-Inf"))))
-    print("dataset class " + str(counter))
-    print("max: ", dataset.max())
-    print("min: ", dataset.min())
-    print("mean: ", dataset.mean())
-    print("len: ", len(dataset))
-    # print("data1 head: ", data1.head())
-    print("head: ", dataset[:10])
-    if len(dataset) < min_len:
-        min_len = len(dataset)
-    datasets.append(dataset)
+        # data1 = pd.read_csv(csv_path1, header=None)
+        # # print("data1 values: ", data1.values)
+        # data1 = np.array(data1.values).squeeze()
 
-print("min_len of the dataset for a class: ", min_len)
-# make sure we have the same number of samples from each class
-for i in range(len(datasets)):
-    datasets[i] = datasets[i][:min_len]
-del min_len
+        dataset = np.genfromtxt(csv_path, delimiter="\n")
+        for expression in ['-inf', '-Inf']:
+            dataset = np.delete(dataset, np.where(dataset == float(expression)))
+        print("dataset class " + str(counter))
+        print("max: ", dataset.max())
+        print("min: ", dataset.min())
+        print("mean: ", dataset.mean())
+        print("len: ", len(dataset))
+        # print("data1 head: ", data1.head())
+        print("head: ", dataset[:10])
+        if len(dataset) < min_len:
+            min_len = len(dataset)
+        datasets.append(dataset)
 
-
-def get_samples(array):
-    with_step = True
-    if with_step:
-        # make more data by overlapping the signals
-        step = sample_size // 4
-        # step = 1
-        samples = []
-        # i - a start index for a sample
-        for i in range(0, len(array) - sample_size, step):
-            samples.append(array[i:i + sample_size])
-        frame = np.array(samples)
-    else:
-        len_final = len(array)
-        len_reminder = len_final % (sample_size * 2)
-        len_final -= len_reminder
-        array = array[:len_final]
-        # cut off the data to the multiple of sample size
-        # take sample_size value and create a row from them
-        frame = array.reshape(-1, sample_size)
-    # shuffle the data
-    np.random.shuffle(frame)
-    return frame
+    print("min_len of the dataset for a class: ", min_len)
+    # make sure we have the same number of samples from each class
+    for i in range(len(datasets)):
+        datasets[i] = datasets[i][:min_len]
+    del min_len
 
 
-# get 2 dimensional datasets
-for i in range(len(datasets)):
-    datasets[i] = get_samples(datasets[i])
-
-# divide into train/test datasets
-stop_train_index = int(len(datasets[0]) * train_rate)
-
-train_arrays = []
-test_arrays = []
-
-for dataset in datasets:
-    train_arrays.append(dataset[:stop_train_index])
-    test_arrays.append(dataset[stop_train_index:])
-
-# find the mean and std of the train data
-train_raw = np.concatenate(train_arrays, axis=0)
-
-mean = train_raw.mean()
-print("train mean value: ", mean)
-
-std = train_raw.std()
-print("train std: ", std)
-
-
-def get_final_data(data, class_number, mean, std):
-    # replace outliers with the mean value
-    count_outliers = np.sum(np.abs(data - mean) > outlier_std_count * std)
-    print(f"count_outliers (for class: {class_number}): ", count_outliers)
-    data[np.abs(data - mean) > outlier_std_count * std] = mean
-    # normalize the data
-    data = (data - mean) / std
-    # create and add column with the class number
-    class_column = np.full((len(data), 1), class_number)
-    data = np.concatenate((class_column, data), axis=1)
-    return data
+    def get_samples(array):
+        with_step = True
+        if with_step:
+            # make more data by overlapping the signals
+            step = max(sample_size // 4, 1)
+            # step = 1
+            samples = []
+            # i - a start index for a sample
+            for i in range(0, len(array) - sample_size, step):
+                samples.append(array[i:i + sample_size])
+            frame = np.array(samples)
+        else:
+            len_final = len(array)
+            len_reminder = len_final % (sample_size * 2)
+            len_final -= len_reminder
+            array = array[:len_final]
+            # cut off the data to the multiple of sample size
+            # take sample_size value and create a row from them
+            frame = array.reshape(-1, sample_size)
+        # shuffle the data
+        np.random.shuffle(frame)
+        return frame
 
 
-train_datasets = []
-for i, array in enumerate(train_arrays):
-    train_datasets.append(get_final_data(array, class_number=i, mean=mean,
-                                         std=std))
-data_train = np.concatenate(train_datasets, axis=0)
-del train_datasets
+    # get 2 dimensional datasets
+    for i in range(len(datasets)):
+        datasets[i] = get_samples(datasets[i])
 
-test_datasets = []
-for i, array in enumerate(test_arrays):
-    test_datasets.append(get_final_data(array, class_number=i, mean=mean,
-                                        std=std))
-data_test = np.concatenate(test_datasets, axis=0)
-del test_datasets
+    # divide into train/test datasets
+    stop_train_index = int(len(datasets[0]) * train_rate)
 
-# print("data train dims: ", data_train.shape)
-# np.savetxt("WIFI_TRAIN", data_train, delimiter=",")
-sample_size = str(sample_size)
-dataset_name = "WIFI_class_" + str(class_counter)
-dir_name = dataset_name + "_sample_" + sample_size
-full_dir = dir_name + "/" + dir_name
+    train_arrays = []
+    test_arrays = []
 
-if not os.path.exists(dir_name):
-    os.makedirs(dir_name)
+    for dataset in datasets:
+        train_arrays.append(dataset[:stop_train_index])
+        test_arrays.append(dataset[stop_train_index:])
 
+    # find the mean and std of the train data
+    train_raw = np.concatenate(train_arrays, axis=0)
 
-def write_data(data_set, file_name):
-    with open(file_name, "w") as f:
-        for row in data_set:
-            # first row is a class number (starting from 0)
-            f.write(str(int(row[0])))
-            # then we have proper values starting from position 1 in each row
-            for value in row[1:]:
-                f.write("," + str(value))
-            f.write("\n")
+    mean = train_raw.mean()
+    print("train mean value: ", mean)
+
+    std = train_raw.std()
+    print("train std: ", std)
 
 
-write_data(data_train, full_dir + "_TRAIN")
-write_data(data_test, full_dir + "_TEST")
+    def get_final_data(data, class_number, mean, std):
+        # replace outliers with the mean value
+        count_outliers = np.sum(np.abs(data - mean) > outlier_std_count * std)
+        print(f"count_outliers (for class: {class_number}): ", count_outliers)
+        data[np.abs(data - mean) > outlier_std_count * std] = mean
+        # normalize the data
+        data = (data - mean) / std
+        # create and add column with the class number
+        class_column = np.full((len(data), 1), class_number)
+        data = np.concatenate((class_column, data), axis=1)
+        return data
 
-print("train mean: ", data_train.mean())
-print("train mean: ", data_test.mean())
+
+    train_datasets = []
+    for i, array in enumerate(train_arrays):
+        train_datasets.append(get_final_data(array, class_number=i, mean=mean,
+                                             std=std))
+    data_train = np.concatenate(train_datasets, axis=0)
+    del train_datasets
+
+    test_datasets = []
+    for i, array in enumerate(test_arrays):
+        test_datasets.append(get_final_data(array, class_number=i, mean=mean,
+                                            std=std))
+    data_test = np.concatenate(test_datasets, axis=0)
+    del test_datasets
+
+    # print("data train dims: ", data_train.shape)
+    # np.savetxt("WIFI_TRAIN", data_train, delimiter=",")
+    sample_size = str(sample_size)
+    dataset_name = "WIFI_class_" + str(class_counter)
+    dir_name = dataset_name + "_sample_" + sample_size
+    full_dir = dir_name + "/" + dir_name
+
+    if not os.path.exists(dir_name):
+        os.makedirs(dir_name)
+
+
+    def write_data(data_set, file_name):
+        with open(file_name, "w") as f:
+            for row in data_set:
+                # first row is a class number (starting from 0)
+                f.write(str(int(row[0])))
+                # then we have proper values starting from position 1 in each row
+                for value in row[1:]:
+                    f.write("," + str(value))
+                f.write("\n")
+
+
+    write_data(data_train, full_dir + "_TRAIN")
+    write_data(data_test, full_dir + "_TEST")
+
+    print("train mean: ", data_train.mean())
+    print("train mean: ", data_test.mean())
