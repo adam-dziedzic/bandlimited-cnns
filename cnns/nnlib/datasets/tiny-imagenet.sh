d8b61bcf3875ded64b86160ae9e34b7da45da8f3
Chameleon Cloud User <cc@g-2.novalocal>
2020-02-09 03:35:01 +0000

    tiny image net

---
+++ cnns/nnlib/datasets/tiny-imagenet.sh
@@ -0,0 +1,10 @@
+o "Testing..."
+wget -nc http://cs231n.stanford.edu/tiny-imagenet-200.zip
+unzip tiny-imagenet-200.zip
+rm -r ./tiny-imagenet-200/test
+python3 val_format.py
+find . -name "*.txt" -delete
+mkdir models
+cp -r tiny-imagenet-200 tiny-224
+python3 resize.py
+
